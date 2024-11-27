a5ef639583dc50f601cd1dff7245f61f05605ef0
Fire <aslslgood22@163.com>
2024-11-27 16:48:09 +0800

    feat: 读取环境配置文件

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,21 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      envFilePath: ['.env.development.local', '.env'],
+      isGlobal: true, // apply to all modules
+    }),
+  ],
   controllers: [AppController],
   providers: [{ provide: 'AppService', useClass: AppService }],
 })
-export class AppModule {}
+export class AppModule {
+  constructor() {
+    console.log(process.env.DATABASE_USER);
+    console.log(process.env.DATABASE_PASSWORD);
+  }
+}
