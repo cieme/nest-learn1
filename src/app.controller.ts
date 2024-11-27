7553c2c82694f23ba96a8516085782f81a45ed3a
Fire <aslslgood22@163.com>
2024-11-27 16:18:46 +0800

    feat: init

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,27 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Inject, Post, Headers } from '@nestjs/common';
 import { AppService } from './app.service';
 
 @Controller()
 export class AppController {
-  constructor(private readonly appService: AppService) {}
+  // constructor(private readonly appService: AppService) {}
+  @Inject('AppService')
+  private readonly appService: AppService;
 
   @Get()
   getHello(): string {
     return this.appService.getHello();
+  }
+
+  @Get('/test')
+  GTest(): string {
+    return 'get' + this.appService.test();
+  }
+
+  @Post('/test')
+  PTest(@Headers() header: Headers): any {
+    return {
+      text: 'post' + this.appService.test(),
+      header,
+    };
   }
 }
